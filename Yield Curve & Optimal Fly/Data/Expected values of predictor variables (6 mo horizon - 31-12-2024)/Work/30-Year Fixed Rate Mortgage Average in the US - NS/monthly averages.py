80d6af14f9c535a0d8a1111e473e9f5fe096b423
BogdanRemusPintilie <127158063+BogdanRemusPintilie@users.noreply.github.com>
2024-09-22 13:22:47 +0100

    Add files via upload

---
+++ Yield Curve & Optimal Fly/Data/Expected values of predictor variables (6 mo horizon - 31-12-2024)/Work/30-Year Fixed Rate Mortgage Average in the US - NS/monthly averages.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\Bogdan\OneDrive - University of Warwick\Desktop\Projects\Yield Curve & Optimal Fly\Data\Expected values of predictor variables (6 mo horizon - 31-12-2024)\Work\30-Year Fixed Rate Mortgage Average in the US\Initial Data.csv")
+
+first_col = "DATE"
+df[first_col] = pd.to_datetime(df[first_col], errors='coerce')
+df.dropna(subset=[first_col], inplace=True)
+df.set_index(first_col, inplace=True)
+
+second_col = "MORTGAGE30US"
+df[second_col] = pd.to_numeric(df[second_col], errors='coerce')
+df.dropna(subset=[second_col], inplace=True)
+monthly_avg = df[second_col].resample('MS').mean()
+
+monthly_avg.to_csv("Done.csv")
